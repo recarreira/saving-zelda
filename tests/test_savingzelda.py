9829e4d14b6b491b482291c2541c309fb71107df
Renata Carreira <re.carreira@gmail.com>
2014-02-16 20:29:12 -0300

    tests/test_savingzelda: adding more test cases for get_page()

--- tests/test_savingzelda.py
+++ tests/test_savingzelda.py
@@ -1,10 +1,20 @@
 import httpretty
 import pytest
+import os
+from bs4 import BeautifulSoup
 from savingzelda import get_page
 
 
+here = os.path.dirname(os.path.abspath(__file__))
+TESTS_DATA = os.path.join(here, 'data')
+
+
+def html_data(file_name):
+  file = open(os.path.join(TESTS_DATA, file_name),'r')
+  return file.read()
+
 @httpretty.activate
-def test_get_page_should_return_proper_html_for_a_200_status_code_page():
+def test_get_page_should_return_body_content_for_a_200_status_code_page():
     httpretty.register_uri(httpretty.GET, "http://github.com/",
                            body="here is the mocked body",
                            status=200)
@@ -20,3 +30,13 @@ def test_get_page_should_return_error_message_for_a_non_200_status_code_page():
         get_page("http://github.com/")
 
     assert "Oops! The page returned a status code 404" == context.value.message
+
+
+@httpretty.activate
+def test_get_page_should_return_proper_html_content():
+    httpretty.register_uri(httpretty.GET, "http://github.com/",
+                           body= html_data("simple.html"),
+                           status=200)
+    soup = BeautifulSoup(get_page("http://github.com/"))
+    assert "Renata Carreira" == soup.title.string
+
