30a2f97b95c2bf952f1b81b47074b585ab6ac1eb
Renata Carreira <re.carreira@gmail.com>
2014-02-16 23:06:55 -0300

    savingzelda/lib: refactoring, now using the SavingZelda class for wrapper

--- savingzelda/lib.py
+++ savingzelda/lib.py
@@ -2,34 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_page(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        return r.text
-    else:
-        raise Exception('Oops! The page returned a status code {status}'.format(status=str(r.status_code)))
+class SavingZelda(object):
 
 
-def get_links(html):
-    list_of_links = []
-    soup = BeautifulSoup(html)
-
-    for link in soup.find_all('a'):
-        list_of_links.append(link.get('href'))
-    return list_of_links
+    def __init__(self, url):
+        self.url = url
 
 
-def check_links(list_of_links):
-    links_and_status = {}
-    for link in list_of_links:
-        response = requests.get(link)
-        links_and_status[link] = response.status_code
-    return links_and_status
+    def get_page(self, url):
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.text
+        else:
+            raise Exception('Oops! The page returned a status code {status}'.format(status=str(r.status_code)))
 
 
-def can_we_save_the_day(links_and_status):
-    alive = True
-    for link in links_and_status:
-        if links_and_status[link] != 200:
-            alive = False
-    return alive
+    def get_links(self, html):
+        list_of_links = []
+        soup = BeautifulSoup(html)
+
+        for link in soup.find_all('a'):
+            list_of_links.append(link.get('href'))
+        return list_of_links
+
+
+    def check_links(self, list_of_links):
+        links_and_status = {}
+        for link in list_of_links:
+            response = requests.get(link)
+            links_and_status[link] = response.status_code
+        return links_and_status
+
+
+    def can_we_save_the_day(self, links_and_status):
+        alive = True
+        for link in links_and_status:
+            if links_and_status[link] != 200:
+                alive = False
+        return alive
--- tests/test_savingzelda.py
+++ tests/test_savingzelda.py
@@ -2,23 +2,28 @@ import httpretty
 import pytest
 import os
 from bs4 import BeautifulSoup
-from savingzelda import get_page, get_links, check_links, can_we_save_the_day
+from savingzelda import SavingZelda
 
 
 here = os.path.dirname(os.path.abspath(__file__))
 TESTS_DATA = os.path.join(here, 'data')
 
+zelda_args = {
+   'url': 'http://github.com',
+}
 
 def html_data(file_name):
     file = open(os.path.join(TESTS_DATA, file_name),'r')
     return file.read()
 
+
 @httpretty.activate
 def test_get_page_should_return_body_content_for_a_200_status_code_page():
-    httpretty.register_uri(httpretty.GET, "http://github.com/",
+    httpretty.register_uri(httpretty.GET, "http://github.com",
                            body="here is the mocked body",
                            status=200)
-    assert "here is the mocked body" == get_page("http://github.com/")
+    saving_zelda = SavingZelda(**zelda_args)
+    assert "here is the mocked body" == saving_zelda.get_page("http://github.com")
 
 
 @httpretty.activate
@@ -26,29 +31,35 @@ def test_get_page_should_return_error_message_for_a_non_200_status_code_page():
     httpretty.register_uri(httpretty.GET, "http://github.com/",
                            body="here is the mocked body",
                            status=404)
+    saving_zelda = SavingZelda(**zelda_args)
+
     with pytest.raises(Exception) as context:
-        get_page("http://github.com/")
+        saving_zelda.get_page("http://github.com/")
 
     assert "Oops! The page returned a status code 404" == context.value.message
 
 
 @httpretty.activate
 def test_get_page_should_return_proper_html_content():
-    httpretty.register_uri(httpretty.GET, "http://github.com/",
+    httpretty.register_uri(httpretty.GET, "http://github.com",
                            body= html_data("simple.html"),
                            status=200)
-    soup = BeautifulSoup(get_page("http://github.com/"))
+    saving_zelda = SavingZelda(**zelda_args)
+
+    soup = BeautifulSoup(saving_zelda.get_page("http://github.com/"))
     assert "Renata Carreira" == soup.title.string
 
 
 def test_get_links_should_return_a_valid_list_of_links():
     body = html_data("simple.html")
-    assert ["http://renatacarreira.com", "https://github.com/recarreira"] == get_links(body)
+    saving_zelda = SavingZelda(**zelda_args)
+    assert ["http://renatacarreira.com", "https://github.com/recarreira"] == saving_zelda.get_links(body)
 
 
 def test_get_links_should_return_empty_list_if_no_links_are_found():
     body = html_data("without-links.html")
-    assert [] == get_links(body)
+    saving_zelda = SavingZelda(**zelda_args)
+    assert [] == saving_zelda.get_links(body)
 
 
 @httpretty.activate
@@ -59,18 +70,24 @@ def test_check_links_should_return_a_dictionary_with_links_and_status():
     httpretty.register_uri(httpretty.GET, "https://github.com/recarreira",
                            body= html_data("simple.html"),
                            status=404)
+    saving_zelda = SavingZelda(**zelda_args)
+
     links = ["http://renatacarreira.com", "https://github.com/recarreira"]
-    assert {"http://renatacarreira.com": 200, "https://github.com/recarreira": 404} == check_links(links)
+    assert {"http://renatacarreira.com": 200, "https://github.com/recarreira": 404} == saving_zelda.check_links(links)
 
 
 def test_check_links_should_return_an_empty_dictionary_given_an_empy_list():
-    assert {} == check_links([])
+    saving_zelda = SavingZelda(**zelda_args)
+    assert {} == saving_zelda.check_links([])
 
 
 def test_dict_with_all_status_code_200_should_save_the_day():
     links_and_status =  {"http://renatacarreira.com": 200, "https://github.com/recarreira": 200}
-    assert True == can_we_save_the_day(links_and_status)
+    saving_zelda = SavingZelda(**zelda_args)
+    assert True == saving_zelda.can_we_save_the_day(links_and_status)
+
 
 def test_dict_with_one_non_200_status_code_should_let_hyrule_down():
     links_and_status = {"http://renatacarreira.com": 404, "https://github.com/recarreira": 200}
-    assert False == can_we_save_the_day(links_and_status)
+    saving_zelda = SavingZelda(**zelda_args)
+    assert False == saving_zelda.can_we_save_the_day(links_and_status)
