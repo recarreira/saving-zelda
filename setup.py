4035f00259747718c1bd72892b21ca12a4fc36f7
rcarreir <re.carreira@gmail.com>
2016-07-22 11:24:59 -0300

    setup.py: release 0.4.1

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ setup(
     author='Renata Carreira',
     author_email='contato@renatacarreira.com',
     url='https://github.com/recarreira/saving-zelda',
-    version='0.4',
+    version='0.4.1',
     zip_safe=False,
     include_package_data=True,
     entry_points={
         'console_scripts':
         ['savingzelda = savingzelda.cli:main'],
-        },
+    },
     packages=[
         'savingzelda',
-        ],
+    ],
     install_requires=[
         'beautifulsoup4==4.3.2',
         'requests==2.2.1',
-        ],
+    ],
 )
